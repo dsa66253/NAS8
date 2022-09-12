c6a93b2e99fbccffb051571590febe5b310dad87
Mary <dsa66253@yahoo.com.tw>
2022-09-12 19:18:01 +0800

    experiment0912

--- data/config.py
+++ data/config.py
@@ -101,7 +101,7 @@ cfg_newnasmodel = {
     'gpu_train': True,
     'batch_size': 64,
     'ngpu': 1,
-    'epoch': 50,
+    'epoch': 100,
     'decay1': 70,
     'decay2': 90,
     'image_size': 128,
--- gitSave.sh
+++ gitSave.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git add .
-git commit -m "experiment0911_3"
+git commit -m "experiment0912"
